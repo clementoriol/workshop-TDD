ba30c5d017a163c87719edeebd0f9c209bd7018e
Clément Oriol <clement@clementoriol.com>
2016-03-21 12:22:54 +0100

    Load jasmine plugins

---
+++ spec/js/jasmine_config.js
@@ -0,0 +1,8 @@
+// Stuff to load for every test
+import $ from 'jquery';
+import 'jasmine-jquery/lib/jasmine-jquery';
+import "babel-polyfill";
+// We're also including 'jasmine-fixtures' directly from the Karma config file
+
+// Tell jasmine where to load the fixtures (commented for now)
+// jasmine.getFixtures().fixturesPath = 'base/spec/js/fixtures';
